0b360dc6e7294f61271cd3dd550e0bd193839c55
imtiazraqib <imtiazud@ualberta.ca>
2019-09-18 16:06:32 -0600

    Day3 code refactor in python

--- 30-Days-of-Code/Day3/intro-to-conditional-statements.py
+++ 30-Days-of-Code/Day3/intro-to-conditional-statements.py
@@ -8,10 +8,13 @@ def weirdOrNah(N):
 
     if (N%2 != 0):
         print("Weird")
+
     elif (N%2 == 0 and 2<= N <=5):
         print("Not Weird")
+
     elif (N%2 == 0 and 6<= N <=20):
         print("Weird")
+        
     elif (N%2 == 0 and N>20):
         print("Not Weird")
     
