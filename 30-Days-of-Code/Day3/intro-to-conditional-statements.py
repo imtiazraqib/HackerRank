50050621e018127028aadb08b32ad1a20424d6e2
imtiazraqib <imtiazud@ualberta.ca>
2019-09-18 16:03:31 -0600

    Day3 added in python

---
+++ 30-Days-of-Code/Day3/intro-to-conditional-statements.py
@@ -0,0 +1,21 @@
+import math
+import os
+import random
+import re
+import sys
+
+def weirdOrNah(N):
+
+    if (N%2 != 0):
+        print("Weird")
+    elif (N%2 == 0 and 2<= N <=5):
+        print("Not Weird")
+    elif (N%2 == 0 and 6<= N <=20):
+        print("Weird")
+    elif (N%2 == 0 and N>20):
+        print("Not Weird")
+    
+
+if __name__ == '__main__':
+    N = int(input())
+    weirdOrNah(N)
